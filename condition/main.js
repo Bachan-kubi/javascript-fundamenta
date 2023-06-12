6ddf82ecd0d457a5a65cf47bde4be256c4fbbfd3
Bachan-kubi <felixghagra@gmail.com>
2023-06-12 20:51:39 +0600

    insal  commit

---
+++ condition/main.js
@@ -0,0 +1,11 @@
+let age = 20;
+if (age < 3) {
+    message = 'Hi, baby!';
+  } else if (age < 18) {
+    message = 'Hello!';
+  } else if (age < 100) {
+    message = 'Greetings!';
+  } else {
+    message = 'What an unusual age!';
+  }
+  console.log(age);
